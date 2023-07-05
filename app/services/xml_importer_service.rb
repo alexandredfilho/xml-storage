3a62bb3d8857c1064bdddc8d4960ef168bf5424d
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-07-04 22:31:23 -0300

    Change xml import service to import new fields

--- app/services/xml_importer_service.rb
+++ app/services/xml_importer_service.rb
@@ -22,8 +22,36 @@ class XmlImporterService
     doc = Nokogiri::XML(open(xml_file))
 
     @parsed_data = {
-      access_key: doc.css("chNFe").text
+      key: doc.css('chNFe').text,
+      company_name: build_company_name_dest(doc.at('dest')),
+      cnpj: build_cnpj_dest(doc.at('dest')),
+      invoice: doc.css('cNF').text,
+      ipi: fetch_ipi(doc.at('imposto', 'IPI')),
+      icms: fetch_icms(doc.at('imposto', 'ICMS', 'ICMS00'))
     }
+  end
+
+  def fetch_icms(xml_row)
+    binding.pry
+    xml_row
+    .css('vICMS')
+    .text
+    .to_f
+  end
+
+  def fetch_ipi(xml_row)
+    xml_row
+    .css('cEnq')
+    .text
+    .to_f
+  end
+
+  def build_company_name_dest(xml_row)
+    xml_row.css('xNome').text
+  end
+
+  def build_cnpj_dest(xml_row)
+    xml_row.css('CNPJ').text
   end
 
   def import_data
